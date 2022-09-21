c5ba013c613c58229028cdb346d9385e3a257b91
JabedH <jabedhtusar@gmail.com>
2022-09-21 16:20:17 +0600

    got user by using id

--- routes/v1/users.route.js
+++ routes/v1/users.route.js
@@ -4,5 +4,5 @@ const toolsControllers = require("../../usersControl/usersControl");
 
 const router = express.Router();
 
-router.route("/").get(toolsControllers.getAllUsers);
+router.route("/:id").get(toolsControllers.getAllUsers);
 module.exports = router;
--- usersControl/usersControl.js
+++ usersControl/usersControl.js
@@ -1,26 +1,27 @@
-// const users = require("../randomUsers/Users.json");
-const users = [
-  {
-    id: 1,
-    gender: "male",
-    name: "jabed",
-    contact: "01789274185",
-    address: "Dhaka",
-    photoUrl: "shorturl.at/bnpWX",
-  },
-  {
-    id: 2,
-    gender: "male",
-    name: "donald trump",
-    contact: "135368476",
-    address: "New York, NY",
-    photoUrl: "shorturl.at/aLOT4",
-  },
-];
+const users = require("../randomUsers/Users.json");
+// const users = [
+//   {
+//     id: 1,
+//     gender: "male",
+//     name: "jabed",
+//     contact: "01789274185",
+//     address: "Dhaka",
+//     photoUrl: "shorturl.at/bnpWX",
+//   },
+//   {
+//     id: 2,
+//     gender: "male",
+//     name: "donald trump",
+//     contact: "135368476",
+//     address: "New York, NY",
+//     photoUrl: "shorturl.at/aLOT4",
+//   },
+// ];
 
 module.exports.getAllUsers = (req, res, next) => {
-  //   let { id } = req.params;
-  //   const newData = users.find((user) => user.id === Number(id));
-  const newData = users;
+  let { id } = req.params;
+  console.log(id);
+  const newData = users.find((user) => user.id === Number(id));
+  //   const newData = users;
   res.send(newData);
 };
